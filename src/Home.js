206051a2d97129d38457238eaf9dc3c96fd27c7c
Mohsen Mokhtari <mohsen.m.sarab@gmail.com>
2020-02-04 13:55:40 +0300

    layout updated

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 import iphone from '../src/assets/iphone.jpg'
+import macbookpro from '../src/assets/macbookpro.jpeg'
 
 const useStyles = makeStyles(theme => ({
     root: {
         flexGrow: 1,
-        paddingTop: 100,
+        paddingTop: 50,
+        marginTop: -40,
 
     },
     paper: {
@@ -21,11 +25,17 @@ const useStyles = makeStyles(theme => ({
     },
     ul: {
         marginBottom: 30,
+
+
     },
     media: {
 
         height: 15,
         paddingBottom: 400,
+    },
+    container: {
+        display: "flex",
+        alignItems: "stretch",
     },
 
 }));
@@ -34,33 +44,88 @@ export default function FullWidthGrid() {
     const classes = useStyles();
 
     return (
-        <div className={classes.root}>
-            <Grid container spacing={3}>
+        <Fragment>
+            <div className={classes.root}>
+                <Grid container spacing={2} elevation={10} style={{ padding: '1.5em' }}>
 
-                <Grid item xs={12} sm={6}>
-                    <Paper className={classes.paper}>
-                        <ul className={classes.ul}>
-                            <li>Fast</li>
-                            <li>User Friendly</li>
-                            <li>Clean design</li>
-                            <li>Responsive</li>
-                        </ul>
+                    <Grid item xs={12} sm={6} container direction='row' spacing={2} alignItems="stretch">
+                        <Paper className={classes.paper} style={{ height: '80%', width: '80%' }}>
+                            <ul className={classes.ul}>
 
-                    </Paper>
+                            </ul>
+                            <ListItem>
+                                <ListItemText>Fast and Responsive</ListItemText>
+                            </ListItem>
+
+                            <ListItem>
+                                <ListItemText>User friendly User Interface</ListItemText>
+                            </ListItem>
+
+                            <ListItem>
+                                <ListItemText>Clean Design</ListItemText>
+                            </ListItem>
+
+                            <ListItem>
+                                <ListItemText>Developed with privacy in mind</ListItemText>
+                            </ListItem>
+
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={6} container direction='row' spacing={2} alignItems="stretch">
+                        <Paper className={classes.paper} style={{ height: '80%', width: '80%' }}>
+                            <Card className={classes.root}>
+                                <CardMedia
+                                    className={classes.media}
+                                    image={iphone}
+                                    title="iphone"
+                                />
+                            </Card>
+                        </Paper>
+                    </Grid>
+
                 </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Paper className={classes.paper}>
-                        <Card className={classes.root}>
-                            <CardMedia
-                                className={classes.media}
-                                image={iphone}
-                                title="iphone"
-                            />
-                        </Card>
-                    </Paper>
-                </Grid>
+            </div >
 
-            </Grid>
-        </div >
+            <div className={classes.root}>
+                <Grid container spacing={2} elevation={10} style={{ padding: '1.5em' }}>
+
+                    <Grid item xs={12} sm={6} container direction='row' spacing={2} alignItems="stretch">
+                        <Paper className={classes.paper} style={{ height: '80%', width: '80%' }}>
+                            <ul className={classes.ul}>
+
+                            </ul>
+                            <ListItem>
+                                <ListItemText>Fast and Responsive</ListItemText>
+                            </ListItem>
+
+                            <ListItem>
+                                <ListItemText>User friendly User Interface</ListItemText>
+                            </ListItem>
+
+                            <ListItem>
+                                <ListItemText>Clean Design</ListItemText>
+                            </ListItem>
+
+                            <ListItem>
+                                <ListItemText>Developed with privacy in mind</ListItemText>
+                            </ListItem>
+
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={6} container direction='row' spacing={2} alignItems="stretch">
+                        <Paper className={classes.paper} style={{ height: '80%', width: '80%' }}>
+                            <Card className={classes.root}>
+                                <CardMedia
+                                    className={classes.media}
+                                    image={macbookpro}
+                                    title="macbookpro"
+                                />
+                            </Card>
+                        </Paper>
+                    </Grid>
+
+                </Grid>
+            </div >
+        </Fragment>
     );
 }
